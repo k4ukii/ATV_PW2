e8da7985df157616b240c99cfa644b27e79147bc
k4ukii <144751465+k4ukii@users.noreply.github.com>
2024-03-19 18:56:54 -0300

    Update exercicio2.php

--- exercicio2.php
+++ exercicio2.php
@@ -1,30 +1,7 @@
-<form action="exercicio2.php" method="post">
+<form action="exe2.php" method="post">
  Insira um Número: <input type =Number name = "numero"><br>
  
  <input type="submit" name ="verificar" value="verificar">
  
- <?php
- 
-$numero = $_POST["numero"];
- if( $numero == 0){
 
- $resultado = "número igual a zero";   
- }
- else if ($numero%3==0){
-
- $resultado = "número impar";
- }
- else{
- $resultado = "número par";
- }
- 
- 
- 
- 
- 
- 
- 
- 
- ?>
- verificação: <input type="text" value= $resultado >
  </form>
