92587b980401b166c87870c9d5842b11ee5791e2
k4ukii <144751465+k4ukii@users.noreply.github.com>
2024-03-19 19:01:21 -0300

    Create exe3.php

---
+++ exe3.php
@@ -0,0 +1,22 @@
+<?php
+ 
+$numero = $_POST["numero"];
+$numero1= $_POST["numero1"];
+ if( $numero > $numero1){
+
+ $resultado = "A maior que B";
+ }
+
+ else{
+ $resultado = "A menor que B";
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ echo $resultado;
+ 
+ ?>
+ 
