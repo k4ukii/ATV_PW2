102b88d454b774bb36574df3542a1d74d17b669e
k4ukii <144751465+k4ukii@users.noreply.github.com>
2024-03-19 19:00:50 -0300

    Update exercicio3.php

--- exercicio3.php
+++ exercicio3.php
@@ -1,30 +1,8 @@
-<form action="exercicio3.php" method="post">
+<form action="exe3.php" method="post">
  Insira um Número (a)  
  <input type =Number name = "numero"><br>
  Insira um Número (b) <input type =Number name = "numero1"><br>
  
  <input type="submit" name ="verificar" value="verificar">
  
- <?php
  
-$numero = $_POST["numero"];
-$numero1= $_POST["numero1"];
- if( $numero > $numero2){
-
- $resultado = "A maior que B";
- }
-
- else{
- $resultado = "A menor que B";
- }
- 
- 
- 
- 
- 
- 
- 
- 
- ?>
- verificação: <input type="text" value= $resultado >
- </form>
