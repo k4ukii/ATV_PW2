15b604ec4e5ed437a1b9b453daf01ce0af9b29e0
k4ukii <144751465+k4ukii@users.noreply.github.com>
2024-03-19 18:49:52 -0300

    Update exercicio1.php

--- exercicio1.php
+++ exercicio1.php
@@ -1,30 +1,8 @@
-<form action="exercicio1.php" method="post">
+<form action="exe1.php" method="post">
  Insira um Número: <input type =Number name = "numero"><br>
  
  <input type="submit" name ="verificar" value="verificar">
  
- <?php
- 
-$numero = $_POST["numero"];
- if( $numero == 0){
-
- $resultado = "número igual a zero";   
- }
- if( $numero >0){
-
- $resultado = "número positivo";   
- }
- else{
- $resultado = "número negativo";
- }
  
  
- 
- 
- 
- 
- 
- 
- ?>
- verificação: <input type="text" value= $resultado >
  </form>
